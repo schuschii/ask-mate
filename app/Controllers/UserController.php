5944fc153b4cb735ee0dfd4dc3347293ebcea513
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-02-05 10:57:04 +0100

    - deleted RegistrationController (using UserController.php instead) - created login function - created saveUser function with validation

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,11 +3,43 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
-
+use App\Model\User;
+use App\Repositories\UserRepository;
 class UserController extends Controller
 {
-    public function showUser($id): void
+    private UserRepository $userRepository;
+
+    public function __construct()
     {
-        echo "User ID: " . htmlspecialchars($id);
+        parent::__construct();
+        $this->userRepository = new UserRepository();
+    }
+
+    public function login(): void
+    {
+        $this->render('auth.login');
+    }
+
+    public function createUser(): void
+    {
+        $this->render('auth.signup');;
+    }
+
+    public function saveUser(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405);
+            die("Method Not Allowed");
+        }
+
+        if (!isset($_POST['email'], $_POST['password'])) {
+            die("Invalid request. Missing fields.");
+        }
+
+        $user = new User($_POST['email'], $_POST['password']);
+        $this->userRepository->save($user);
+
+        header("Location: /home");
+        exit;
     }
 }
