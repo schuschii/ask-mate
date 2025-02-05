d6b099083b15d5d4318587b5e6bd59e0493ccc01
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2025-02-05 10:57:54 +0100

    Added to model: user.php

---
+++ app/Model/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Model;
+
+use DateTime;
+
+class User
+{
+    private string $email;
+    private string $password;
+
+    private DateTime $registration_time;
+
+    public function __construct(string $email, string $password)
+    {
+        $this->email = $email;
+        $this->password = $password;
+        $this->registration_time = new DateTime('now');
+    }
+
+    public function getRegistrationTime(): DateTime
+    {
+        return $this->registration_time;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+}
